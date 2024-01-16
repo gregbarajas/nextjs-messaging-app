fa06f875cb72cce84bc13a35a27a5358575de9ca
Greg Barajas <gregbarajas96@gmail.com>
2024-01-15 20:33:44 -0800

    Added next-auth/react

--- src/app/(site)/components/AuthForm.tsx
+++ src/app/(site)/components/AuthForm.tsx
@@ -1,11 +1,63 @@
 'use client';
+import React from 'react';
+import axios from "axios";
+import { signIn, useSession } from 'next-auth/react';
+import { useCallback, useEffect, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { useRouter } from "next/navigation";
+
+type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => { 
+    const router = useRouter();
+
+
+    const [variant, setVariant] = useState<Variant>('LOGIN');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleVariant = useCallback(() => {
+        if (variant === 'LOGIN') {
+          setVariant('REGISTER');
+        } else {
+          setVariant('LOGIN');
+        }
+      }, [variant]);
+
+      const {
+        register,
+        handleSubmit,
+        formState: {
+          errors,
+        }
+      } = useForm<FieldValues>({
+        defaultValues: {
+          name: '',
+          email: '',
+          password: ''
+        }
+      });
+
+      const socialAction = (action: string) => {
+        setIsLoading(true);
+      
+    
+    
+
     return (
-        <div>
-            Auth Form
+        <div 
+        className="
+        bg-white
+          px-4
+          py-8
+          shadow
+          sm:rounded-lg
+          sm:px-10
+        "
+      >
+            Auth Form2
         </div>
     )
  }
+}
 
  export default AuthForm;
